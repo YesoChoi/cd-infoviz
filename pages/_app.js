c42206820308e520a07f0ead077e18f5ad304266
Yeso Choi <yeso_choi@Yesoui-MacBookPro.local>
2024-12-06 05:08:45 +0900

    Delete: map v3 folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "@/styles/common/globals.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head><title>Behind my Nike shoes</title></Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
