8d6da0d45cd871ea07fc43ecb6b604875b9453b1
Yeso Choi <yeso_choi@Yesoui-MacBookPro.local>
2024-12-09 22:10:51 +0900

    Fix: Gradient Background Color

--- components/map/v1/background/bg-color.js
+++ components/map/v1/background/bg-color.js
@@ -1,18 +1,32 @@
-
 import React from 'react'
 import * as THREE from 'three'
 
 const BackgroundColor = () => {
+  const vertexShader = `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `
+
+  const fragmentShader = `
+    varying vec2 vUv;
+    void main() {
+      vec3 topColor = vec3(0.4, 0.6, 1.0); // 하늘색
+      vec3 bottomColor = vec3(1.0, 1.0, 0.8); // 연한 노란색
+      vec3 finalColor = mix(bottomColor, topColor, vUv.y);
+      gl_FragColor = vec4(finalColor, 1.0);
+    }
+  `
+
   return (
     <mesh scale={100}>
       <sphereGeometry args={[1, 64, 64]} />
-      <meshPhongMaterial
+      <shaderMaterial
         side={THREE.BackSide}
-        color="#444466"
-        emissive="#2233ff"
-        emissiveIntensity={0.2}
-        transparent
-        opacity={1}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
       />
     </mesh>
   )
