c5ff4c125854abcd7deb339902e304212fc4dc41
Yeso Choi <yeso_choi@Yesoui-MacBookPro.local>
2024-12-09 15:21:21 +0900

    Fix: Set the map page as the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { useState, useEffect } from 'react'
+import MapScene from '@/components/map/v1'
+import Intro from '@/components/map/v1/intro'
+import * as THREE from 'three'
 
 const GlobalStyle = createGlobalStyle`
   html, body {
@@ -15,28 +19,46 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const LandingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: #f0f0f0;
-  font-family: Arial, sans-serif;
-`;
+const mapUrl = '/texture/map_texture.png'
 
-const Message = styled.h1`
-  font-size: 2rem;
-  color: #333;
-`;
+const V3Page = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isIntroComplete, setIsIntroComplete] = useState(false)
 
-export default function Home() {
+  useEffect(() => {
+    // 인트로 애니메이션 지속 시간
+    const introDuration = 12 * 1000 
+    const timer = setTimeout(() => {
+      setIsIntroComplete(true)
+    }, introDuration)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  useEffect(() => {
+    if (isIntroComplete) {
+      // 실제 로딩이 완료되면 인트로가 끝난 후에만 메인 페이지로 전환
+      const textureLoader = new THREE.TextureLoader()
+      textureLoader.load(mapUrl, () => {
+        setIsLoading(false)
+      })
+    }
+  }, [isIntroComplete])
+
+  // Skip 버튼 핸들러 추가
+  const handleSkip = () => {
+    setIsIntroComplete(true)
+  }
+
   return (
-    <>
-      <GlobalStyle />
-      <LandingContainer>
-        <Message>작업중</Message>
-      </LandingContainer>
-    </>
-  );
+    <div style={{ height: '100%', overflow: 'hidden' }}>
+      {isLoading ? (
+        <Intro onSkip={handleSkip} />
+      ) : (
+        <MapScene mapUrl={mapUrl} />
+      )}
+    </div>
+  )
 }
+
+export default V3Page
